4cc4db9004d9bc2a172c0e43583ca76eaf4e8985
Bruno <bop1990@gmail.com>
2018-12-21 00:52:55 -0200

    instrucoes de execucao

--- src/main/java/br/com/linkdata/myApp/MyAppApplication.java
+++ src/main/java/br/com/linkdata/myApp/MyAppApplication.java
@@ -4,35 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyAppApplication 
-//implements CommandLineRunner 
-{
-
-//	@Autowired
-//	public EmpresaService empresaService;
+public class MyAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppApplication.class, args);
 	}
-	
-//	@RequestMapping("/")
-//	@ResponseBody
-//	public String ola(){
-//		System.out.println(">>>>>>>>>>>>>>>>>> teste");
-//		return "Página inicial.</br>";
-//
-//	}
-
-//	@Override
-//	public void run(String... args) throws Exception {
-//		try{
-//			List<Empresa> empresas = empresaService.getEmpresas();
-//			EmpresaREL relatorio = new EmpresaREL();
-//			relatorio.imprimir(empresas);
-//		}
-//		catch(Exception e){
-//			System.out.println(e.getMessage());
-//		}
-//	}
 
 }
